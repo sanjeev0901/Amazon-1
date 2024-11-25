22e61ab46def89683ba2180c9dbcf737d8854d71
Sanjeev <sanjeevbalaji09@gmail.com>
2024-11-25 18:29:13 +0530

    Modify setItems fn

--- scripts/cart.js
+++ scripts/cart.js
@@ -2,10 +2,12 @@ const cart = {
   cartItems: [],
   setItemsInCart(button, quantity = 1) {
     const { id } = button.dataset;
-    this.cartItems.push({
-      id,
-      quantity,
-    });
+    let matchingItem = this.cartItems.find((item) => item.id === id);
+    if (matchingItem) {
+      matchingItem.quantity += quantity;
+    } else {
+      this.cartItems.push({ id, quantity });
+    }
   },
 };
 export default cart;
