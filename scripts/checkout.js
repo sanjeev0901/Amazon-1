2c2dcb76cc429d787da8212906d59343511ba682
Sanjeev <sanjeevbalaji09@gmail.com>
2024-11-29 13:12:23 +0530

    Create getProduct function in checkout-page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,14 @@
+import { products } from "../data/products.js";
 import cart from "./cart.js";
 
-console.log(cart);
+const getProduct = (product) => {
+  const extractedProduct = products.find((item) => {
+    return item.id == product.id;
+  });
+  extractedProduct.quantity = product.quantity;
+  return extractedProduct;
+};
+
+console.log(
+  getProduct({ id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 2 })
+);
